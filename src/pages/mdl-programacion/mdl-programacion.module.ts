8e5b0f208a499a6835b7f7035a26965bf83f51d7
OscarAguilarP <42579936+OscarAguilarP@users.noreply.github.com>
2018-08-21 13:29:33 -0300

    Update mdl-programacion.module.ts

--- src/pages/mdl-programacion/mdl-programacion.module.ts
+++ src/pages/mdl-programacion/mdl-programacion.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MdlProgramacionPage } from './mdl-programacion';
+import { NavController, Nav } from 'ionic-angular';
+
 
 @NgModule({
   declarations: [
