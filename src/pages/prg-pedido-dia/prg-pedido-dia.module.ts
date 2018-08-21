32f0f969b54f7e6af71c0a243511216bbf2be274
OscarAguilarP <42579936+OscarAguilarP@users.noreply.github.com>
2018-08-21 13:35:01 -0300

    Update prg-pedido-dia.module.ts

--- src/pages/prg-pedido-dia/prg-pedido-dia.module.ts
+++ src/pages/prg-pedido-dia/prg-pedido-dia.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PrgPedidoDiaPage } from './prg-pedido-dia';
+import { NavController, Nav } from 'ionic-angular';
+
 
 @NgModule({
   declarations: [
